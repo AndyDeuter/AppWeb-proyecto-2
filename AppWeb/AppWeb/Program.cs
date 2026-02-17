90f2a74657ff8fcddd17b930f902425fdb478c4e
Andy <Andydeuter2021@gmail.com>
2026-02-17 12:32:37 -0600

    ya funciona la conecion sql

--- AppWeb/AppWeb/Program.cs
+++ AppWeb/AppWeb/Program.cs
@@ -1,5 +1,5 @@
 using AppWeb.Data;
-using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- AppWeb/AppWeb/obj/Release/net8.0/AppWeb.AssemblyInfo.cs
+++ AppWeb/AppWeb/obj/Release/net8.0/AppWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63dd9c47d5fd198d19eb3d4709dfcedbac44c8a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e409aadd0f6dd77f518fe9e69b05cc48ec70a8ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
